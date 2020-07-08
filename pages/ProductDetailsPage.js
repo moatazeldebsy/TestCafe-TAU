e2efa13e1b528a3833643b1471434736379d07bc
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2020-07-08 16:53:27 +0200

    Update ProductDetailsPage.js

--- pages/ProductDetailsPage.js
+++ pages/ProductDetailsPage.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 class ProductDetailsPage{
     constructor() {
-        this.productPrice = Selector("input[id='price-value-4']").withText('$1,800.00')
+        this.productPrice = Selector("span[id='price-value-4']").withText('$1,800.00')
         this.prductQuantity = Selector('input#product_enteredQuantity_4.qty-input')
         this.addToCart = Selector("input[id='add-to-cart-button-4']")
         this.successMessage = Selector('div.bar-notification.success');
