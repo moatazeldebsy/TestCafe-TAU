215434960ae13cc87069c9925ea343b5f7b78c23
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2020-07-16 22:59:50 +0200

    Update ClientFunctionsTest.js

--- tests/ClientFunctionsTest.js
+++ tests/ClientFunctionsTest.js
@@ -1,18 +1,19 @@
-import { Selector } from 'testcafe';
-import { ClientFunction } from 'testcafe';
+import {Selector,ClientFunction} from 'testcafe';
 
-fixture("Test Fixture with TestCafe")
+const developerName = Selector("#developer-name");
+const osOption = Selector("#macos");
+const submitButton = Selector("#submit-button");
+
+const getPageURL = ClientFunction(() => window.location.href); 
+
+
+fixture("First Fixture")
     .page("https://devexpress.github.io/testcafe/example/");
-    
-    const developerName = Selector('#developer-name'); 
-    const getPageURL = ClientFunction(() => window.location.href);
 
-    test("Client Function Test with TestCafe", async t => {
-        await t
-            .expect(developerName.value).eql('','input is empty')
-            .typeText(developerName,'TAU')
-            .expect(developerName.value).eql('TAU','input contains "TAU"')
-            .click('input#macos')
-            .click('#submit-button') 
-            .expect(getPageURL()).contains('thank-you');       
-        });
+test("First Test", async t => {
+ await t
+        .typeText(developerName,"TAU")
+        .click(osOption)
+        .click(submitButton)
+        .expect(getPageURL()).contains('thank-you');
+});
