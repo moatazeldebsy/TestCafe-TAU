0c98a880fdcf2e3271c86b77bcddd1a047d6ab02
Moataz Nabil <45570191+moatazeldebsy@users.noreply.github.com>
2020-07-16 22:59:19 +0200

    Update HoverTest.js

--- tests/HoverTest.js
+++ tests/HoverTest.js
@@ -1,9 +1,13 @@
-import { Selector } from 'testcafe';
-
-fixture `Example`
-    .page `https://js.devexpress.com`;
+fixture("Hover Fixture")
+.page("https://devexpress.github.io/testcafe/example/")
+.beforeEach(async t =>{
+    await t
+     .maximizeWindow()
+     .setPageLoadTimeout(0)
+     .setTestSpeed(0.1);
+});
 
 test('Hover test', async t => {
     await t
-        .hover('.map-container');
+        .hover('input#populate');
 });
