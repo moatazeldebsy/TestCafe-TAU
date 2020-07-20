0da0a828c8888bc4dc9dbf0191553ca12ef9a18e
moatazeldebsy <moataznabil2019@gmail.com>
2020-07-20 16:31:44 +0200

    fix typo

--- tests/SetPageLoadTimeout.js
+++ tests/SetPageLoadTimeout.js
@@ -1,7 +1,7 @@
-fixture("Set page timout Fixture")
+fixture("Set page timeout Fixture")
     .page("http://devexpress.github.io/testcafe/");
 
-test("set page timout Test", async t =>{
+test("set page timeout Test", async t =>{
     await t
     .setPageLoadTimeout(0)
     .navigateTo('http://devexpress.github.io/testcafe/');
